243b19aef5e9187c178fc8c28c89634cea15a8d4
RobertoDeAbreuSalgado <r.salgado3@gmail.com>
2020-12-16 19:35:03 -0300

    Aumentando o limite da Request json

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,12 @@
 import express, { Express } from "express";
 import cors from "cors";
-import bodyParser from "body-parser";
 import {AddressInfo} from "net";
 import { userRouter } from "./routes/userRouter";
 import { musicRouter } from "./routes/musicRouter";
 
 const app: Express = express();
-app.use(express.json());
+app.use(express.json({limit: "500mb"}));
 app.use(cors());
-
-app.use(bodyParser.json({limit: "500mb"}));
-app.use(bodyParser.urlencoded({
-  limit: "500mb",
-  parameterLimit: 500000000,
-  extended: true
-}));
 
 app.use("/user", userRouter);
 app.use("/music", musicRouter);
