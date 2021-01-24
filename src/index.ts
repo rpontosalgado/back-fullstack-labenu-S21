8796fba4ddf5cdcdcf307a748ad78964d69f5eee
RobertoDeAbreuSalgado <r.salgado3@gmail.com>
2021-01-23 23:40:29 -0300

    Alterando rotas dos endpoints

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import {AddressInfo} from "net";
 import { userRouter } from "./routes/userRouter";
 import { musicRouter } from "./routes/musicRouter";
 import { playlistRouter } from "./routes/playlistRouter";
+import MusicController from "./controller/MusicController";
 
 const app: Express = express();
 app.use(express.json({limit: "500mb"}));
@@ -12,6 +13,10 @@ app.use(cors());
 app.use("/user", userRouter);
 app.use("/music", musicRouter);
 app.use("/playlist", playlistRouter);
+
+app.get("/genres", MusicController.getGenreNames);
+app.get("/albums", MusicController.getAlbums);
+app.get("/artists", MusicController.getArtistNames);
 
 const server = app.listen(process.env.PORT || 3003, () => {
   if (server) {
--- src/routes/musicRouter.ts
+++ src/routes/musicRouter.ts
@@ -4,7 +4,4 @@ import MusicController from "../controller/MusicController";
 export const musicRouter: Router = express.Router();
 
 musicRouter.post("/", MusicController.createMusic);
-musicRouter.get("/genres", MusicController.getGenreNames);
-musicRouter.get("/albums", MusicController.getAlbums);
-musicRouter.get("/artists", MusicController.getArtistNames);
 musicRouter.get("/:id?", MusicController.getMusic);
